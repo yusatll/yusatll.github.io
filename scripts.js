f62b7c3be6b59ca36001f35dffdf702f9f8e5dda
Ahmet Yusa Telli <yusahmetelli@gmail.com>
2024-08-29 13:17:19 +0300

    Update scripts.js

--- scripts.js
+++ scripts.js
@@ -1,5 +1,3 @@
-const YOUTUBE_API_KEY = 'AIzaSyDoNPXeCZqmnRkkEhOT0i3XdGoX23YR59A';
-const CHANNEL_ID = 'UCbp2rjb5uIbvze1WkOZtLug';
 
 async function fetchYouTubeVideos() {
     const response = await fetch(`https://www.googleapis.com/youtube/v3/search?key=${YOUTUBE_API_KEY}&channelId=${CHANNEL_ID}&part=snippet,id&order=date&maxResults=5`);
